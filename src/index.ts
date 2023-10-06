65c86ceb6a80b099aad6fd4af33fbbfa5b29d5c4
hariyebk <harunbekri6@gmail.com>
2023-10-06 16:18:58 +0300

    man united total wins

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,15 @@ const matches = fs.readFileSync("football.csv" , {
     encoding: "utf-8"
 }).split("\n").map((match : string) : string[] => match.split(","))
 
-console.log(matches)
+let manuitedHomewins = 0
+let manunitedAwaywins = 0
+for(let match of matches){
+    if(match[1] === "Man United" && match[5] === "H"){
+        manuitedHomewins++
+    }
+    else if(match[2] === "Man United" && match[5] === "A"){
+        manunitedAwaywins++
+    }
+}
+
+console.log(`Man United has won A total of ${manuitedHomewins + manunitedAwaywins} games (Home ${manuitedHomewins}, Away ${manunitedAwaywins})`)
