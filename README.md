3a009b8260639227273498fcc69d3dcc1d449924
hariyebk <harunbekri6@gmail.com>
2023-10-06 17:35:19 +0300

    create README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# English Premier League 2018/2019 Match Analysis
+
+This project is designed to analyze the match results of the English Premier League for the 2018/2019 season. It takes input data from a CSV file containing match details and generates a comprehensive report summarizing the outcomes and statistics of the matches.
+
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,19 @@ const matches = fs.readFileSync("football.csv" , {
     encoding: "utf-8"
 }).split("\n").map((match : string) : string[] => match.split(","))
 
+// enumeration
+enum MatchResult{
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw =  "D"
+}
 let manuitedHomewins = 0
 let manunitedAwaywins = 0
 for(let match of matches){
-    if(match[1] === "Man United" && match[5] === "H"){
+    if(match[1] === "Man United" && match[5] === MatchResult.HomeWin){
         manuitedHomewins++
     }
-    else if(match[2] === "Man United" && match[5] === "A"){
+    else if(match[2] === "Man United" && match[5] === MatchResult.AwayWin){
         manunitedAwaywins++
     }
 }
