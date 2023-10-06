68e3a6b7e0c759e26fc7abc4683cc769c75e1b21
hariyebk <harunbekri6@gmail.com>
2023-10-06 16:02:19 +0300

    parsed csv data

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import { match } from "assert"
 import fs from "fs"
 
+const matches = fs.readFileSync("football.csv" , {
+    encoding: "utf-8"
+}).split("\n").map((match : string) : string[] => match.split(","))
+
+console.log(matches)
